03ed7b3103c8c2e93920c5ff4e6fa38e6847fc59
Jonathan Uhler <jonathan@uhler.com>
2020-02-28 18:55:35 -0800

    Initial Commit

---
+++ Fitness/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fitness
+//
+//  Created by Jonathan Uhler on 2/28/20.
+//  Copyright © 2020 Jonathan Uhler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
